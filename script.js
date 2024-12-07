256eb55421ef2f1efc625f79d40034d592f74dcc
Tharushika Thilakarathna <146727574+tharushika2002@users.noreply.github.com>
2024-12-07 20:36:15 +0530

    change and create some files

--- script.js
+++ script.js
@@ -39,3 +39,47 @@ function eraseText(){
     }
 }
 window.onload = typeWriter
+
+
+const navLinks = document.querySelectorAll('.links a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); 
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId); 
+        
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth', 
+                block: 'start' 
+            });
+        }
+    });
+});
+
+
+document.addEventListener('scroll', function () {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('.nav-container .link');
+
+    sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop - 100 && window.scrollY < sectionTop + sectionHeight - 100) {
+            navLinks.forEach(link => link.classList.remove('active'));
+            navLinks[index].classList.add('active');
+        }
+    });
+});
+
+
+document.querySelectorAll('.nav-container .link').forEach(link => {
+    link.addEventListener('click', function () {
+        
+        document.querySelectorAll('.nav-container .link').forEach(link => link.classList.remove('active'));
+
+        this.classList.add('active');
+    });
+});
